1ef6fae0e3cd55ce88cef919e431abc8ad5bb467
Sam Nachman <shnachman@gmail.com>
2020-10-25 14:40:05 -0400

    corrected number of days

--- themooch.py
+++ themooch.py
@@ -4,5 +4,5 @@ def scaramuccis(first, second, roundto=100):
     minutes = seconds / 60
     hours = minutes / 60
     days = hours / 24
-    scaramuccis = days/11
+    scaramuccis = days/10
     return round(scaramuccis, roundto)
